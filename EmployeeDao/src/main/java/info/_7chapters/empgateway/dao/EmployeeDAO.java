5da35f929d5f63631b5ae38fcd595c19422265af
Jayram <jayram.rout@7chapters.info>
2014-10-25 10:15:42 -0400

    Update

--- EmployeeDao/src/main/java/info/_7chapters/empgateway/dao/EmployeeDAO.java
+++
@@ -1,5 +0,0 @@
-package info._7chapters.empgateway.dao;
-
-public class EmployeeDAO {
-
-}
--- EmployeeWar/src/main/java/info/_7chapters/hr/ws/HolidayEndpoint.java
+++ EmployeeWar/src/main/java/info/_7chapters/hr/ws/HolidayEndpoint.java
@@ -3,54 +3,50 @@ package info._7chapters.hr.ws;
 import info._7chapters.hr.schemas.HolidayRequest;
 import info._7chapters.hr.service.HumanResourceService;
 
-import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.jdom2.Element;
 import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
-import org.jdom2.filter.Filters;
+//import org.jdom2.Namespace;
+//import org.jdom2.filter.Filters;
 import org.jdom2.xpath.XPathExpression;
-import org.jdom2.xpath.XPathFactory;
+//import org.jdom2.xpath.XPathFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.XPathParam;
+//import org.springframework.ws.server.endpoint.annotation.XPathParam;
 import org.springframework.ws.soap.SoapHeader;
-import org.springframework.ws.transport.context.TransportContext;
-import org.springframework.ws.transport.context.TransportContextHolder;
-import org.springframework.ws.transport.http.HttpServletConnection;
+//import org.springframework.ws.transport.context.TransportContext;
+//import org.springframework.ws.transport.context.TransportContextHolder;
+//import org.springframework.ws.transport.http.HttpServletConnection;
+//import javax.servlet.http.HttpServletRequest;
+import org.jdom2.Element;
 
 @Endpoint
 public class HolidayEndpoint {
 	private static final String NAMESPACE_URI = "http://_7chapters.info/hr/schemas";
 
-    private XPathExpression<Element> startDateExpression;
-
+/*    private XPathExpression<Element> startDateExpression;
     private XPathExpression<Element> endDateExpression;
-
     private XPathExpression<Element> firstNameExpression;
-    
     private XPathExpression<Element> lastNameExpression;
-    private HumanResourceService humanResourceService;
+*/    
+	private HumanResourceService humanResourceService;
     
                                                                               
     @Autowired
     public HolidayEndpoint(HumanResourceService humanResourceService) throws JDOMException {
         this.humanResourceService = humanResourceService;
 
-        Namespace namespace = Namespace.getNamespace("hr", NAMESPACE_URI);
+/*        Namespace namespace = Namespace.getNamespace("hr", NAMESPACE_URI);
         XPathFactory xPathFactory = XPathFactory.instance();
         startDateExpression = xPathFactory.compile("//hr:StartDate", Filters.element(), null, namespace);
         endDateExpression = xPathFactory.compile("//hr:EndDate", Filters.element(), null, namespace);
         firstNameExpression = xPathFactory.compile("//hr:FirstName", Filters.element(), null, namespace);
         lastNameExpression = xPathFactory.compile("//hr:LastName", Filters.element(), null, namespace);
-    }
+*/    }
 
    /* @PayloadRoot(namespace = NAMESPACE_URI, localPart = "HolidayRequest")     
     public void handleHolidayRequest(@RequestPayload Element holidayRequest) throws Exception {                        
